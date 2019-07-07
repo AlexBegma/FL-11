9c4403bad96d7aadfe2e40023ca85aff53793e25
alexbehma <alexbegma@gmail.com>
2019-07-07 20:15:51 +0300

    07.07

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,16 @@
-// Your code goes here
+let coordinateAx = prompt('What is cordinate Ax ?');
+let coordinateAy = prompt('What is cordinate Ay ?');
+let coordinateBx = prompt('What is cordinate Bx ?');
+let coordinateBy = prompt('What is cordinate By ?');
+let coordinateCx = prompt('What is cordinate Cx ?');
+let coordinateCy = prompt('What is cordinate Cy ?');
+
+let devider = 2;
+let centerX = (Number(coordinateAx) + Number(coordinateBx)) / devider;
+let centerY = (Number(coordinateAy) + Number(coordinateBy)) / devider;
+
+if (Number(coordinateCx) === centerX && Number(coordinateCy) === centerY) {
+	console.log(true);
+} else {
+	console.log(false);
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let lengthAString = prompt('What is the triangle side length A ?');
+let lengthBString = prompt('What is the triangle side length B ?');
+let lengthCString = prompt('What is the triangle side length C ?');
+
+let lengthA = Number(lengthAString);
+let lengthB = Number(lengthBString);
+let lengthC = Number(lengthCString);
+
+if (lengthA > 0 && lengthB > 0 && lengthC > 0
+	&& lengthA + lengthB > lengthC && lengthA + lengthC > lengthB && lengthB + lengthC > lengthA) {
+	if (lengthA === lengthB && lengthA === lengthC && lengthB === lengthC) {
+		console.log('Eequivalent triangle');
+	} else if (lengthA === lengthB || lengthA === lengthC || lengthC === lengthB) {
+		console.log('Isosceles triangle');
+	} else if (lengthA !== lengthB && lengthA !== lengthC && lengthB !== lengthC) {
+		console.log('Normal triangle');
+	}
+} else {
+	console.log('Triangle doesn’t exist');
+}
